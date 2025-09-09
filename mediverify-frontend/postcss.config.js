e7a3c5d6ca0ce8501706af1cc8d051057bf30821
Ruth <146713899+Ruthneemu@users.noreply.github.com>
2025-09-09 10:59:19 +0000

    all

--- mediverify-frontend/postcss.config.js
+++ mediverify-frontend/postcss.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   plugins: {
-    // Tailwind CSS plugin for PostCSS
-    tailwindcss: {},
-    // Autoprefixer adds vendor prefixes to CSS rules for browser compatibility
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
-};
-
+}
