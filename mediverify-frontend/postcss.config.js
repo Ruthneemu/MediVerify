fba0ce2419e0badc9475c2f03f3691149bd011cf
ruthneemu <ruthneemu@gmail.com>
2025-10-06 12:56:27 +0000

    all

--- mediverify-frontend/postcss.config.js
+++ mediverify-frontend/postcss.config.js
@@ -1,7 +1,7 @@
+// postcss.config.js
 module.exports = {
       plugins: {
-            '@tailwindcss/postcss': {}, // Use the new plugin
-                autoprefixer: {},
-      }
-}
-      
+        '@tailwindcss/postcss': {},
+        autoprefixer: {},
+      },
+    }
